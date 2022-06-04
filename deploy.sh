06622ae009a46cfbc81e560a278c317dd6f1a7b9
Comic.Ivans <isalidocobo@gmail.com>
2022-06-05 01:06:29 +0200

    Preparativos y aclaraciones

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+# Abort on errors
+set -e
+
+# Build
+npm run docs:build
+
+# Navigate into the build output directory
+cd docs/.vitepress/dist
+
+git init
+git add -a
+git commit -m 'Actualización de la documentación'
+
+git push -f git@github.com:ComicIvans/server.git main:gh-pages
+
+cd -
---
+++ docs/.vitepress/config.js
@@ -0,0 +1,6 @@
+export default {
+    title: 'Servidor mamadísimo',
+    description: 'Configuración del servidor.',
+    lang: 'es-ES',
+    base: '/server/',
+  }
